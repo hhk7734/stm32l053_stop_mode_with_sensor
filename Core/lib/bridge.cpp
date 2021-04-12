51d014bddcdad56b3961c7a14b152689d0dea34a
Hyeonki Hong <hhk7734@gmail.com>
2021-04-12 16:30:16 +0900

    Core: lib: add `_write()`

--- Core/lib/bridge.cpp
+++ Core/lib/bridge.cpp
@@ -1,5 +1,8 @@
 #include "bridge.h"
 
+#include "gpio.h"
+#include "usart.h"
+
 void setup() {
     // C/C++ 코드 사용
 }
@@ -8,4 +11,14 @@ void loop() {
     // C/C++ 코드 사용
     HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
     HAL_Delay(200);
+}
+
+#ifdef __cplusplus
+extern "C" int _write(int32_t file, uint8_t *ptr, int32_t len) {
+#else
+int _write(int32_t file, uint8_t *ptr, int32_t len) {
+#endif
+    if(HAL_UART_Transmit(&huart2, ptr, len, len) == HAL_OK) return len;
+    else
+        return 0;
 }
